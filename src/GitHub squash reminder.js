f37aa8c3f21ce9392e24883626efecbab26f315c
George Gillams <george.gillams@skyscanner.net>
2020-03-10 18:22:08 +0000

    [NO-JIRA] Fix CI

--- src/GitHub squash reminder.js
+++ src/GitHub squash reminder.js
@@ -10,53 +10,53 @@
 // @description   	Adds an reminder to squash PRs that have > 1 commit
 // ==/UserScript==
 
-let lastModifiedPr = null;
-
 function addReminder() {
-    const currentReminder = document.getElementById('squash_reminder_button');
-    console.log(`currentReminder`, currentReminder);
-    if (currentReminder) { return; }
+  const currentReminder = document.getElementById('squash_reminder_button');
+  console.log(`currentReminder`, currentReminder);
+  if (currentReminder) {
+    return;
+  }
 
-    const commitCount = parseInt(
-      document.getElementById('commits_tab_counter').textContent,
-      10,
-    );
-    if (commitCount !== 1) {
-      let allElements = document.getElementsByTagName('DIV');
-      for (let i = 0; i < allElements.length; i += 1) {
-        const element = allElements[i];
-        if (element.className === 'merge-message') {
-          element.innerHTML = `<div style="display: block;margin-bottom: -2rem;text-decoration: none;font-weight: bold;font-size: 1.5rem;font-family: Quattrocento Sans,sans-serif;color: #e02626;">REMEMBER TO SQUASH!</div> <br/> <br/>${element.innerHTML}`;
-        }
-      }
-      allElements = document.getElementsByTagName('BUTTON');
-      for (let i = 0; i < allElements.length; i += 1) {
-        const element = allElements[i];
-        if (
-          element.textContent &&
-          element.textContent.includes('Merge pull request')
-        ) {
-          element.disabled = true;
-          element.textContent = 'MERGE ONLY ONCE SQUASHED!';
-          element.id = "squash_reminder_button";
-          element.style.backgroundImage =
-            'linear-gradient(-180deg, #e02626 0%, #9F2D27 90%)';
-          element.style.color = 'white';
-        }
-        // if (
-        //   element.textContent &&
-        //   element.textContent.includes('Squash and merge')
-        // ) {
-        //   element.textContent = 'SMERGE!';
-        // }
-        // if (
-        //   element.textContent &&
-        //   element.textContent.includes('Confirm squash and merge')
-        // ) {
-        //   element.textContent = 'Confirm SMERGE!';
-        // }
+  const commitCount = parseInt(
+    document.getElementById('commits_tab_counter').textContent,
+    10,
+  );
+  if (commitCount !== 1) {
+    let allElements = document.getElementsByTagName('DIV');
+    for (let i = 0; i < allElements.length; i += 1) {
+      const element = allElements[i];
+      if (element.className === 'merge-message') {
+        element.innerHTML = `<div style="display: block;margin-bottom: -2rem;text-decoration: none;font-weight: bold;font-size: 1.5rem;font-family: Quattrocento Sans,sans-serif;color: #e02626;">REMEMBER TO SQUASH!</div> <br/> <br/>${element.innerHTML}`;
       }
     }
+    allElements = document.getElementsByTagName('BUTTON');
+    for (let i = 0; i < allElements.length; i += 1) {
+      const element = allElements[i];
+      if (
+        element.textContent &&
+        element.textContent.includes('Merge pull request')
+      ) {
+        element.disabled = true;
+        element.textContent = 'MERGE ONLY ONCE SQUASHED!';
+        element.id = 'squash_reminder_button';
+        element.style.backgroundImage =
+          'linear-gradient(-180deg, #e02626 0%, #9F2D27 90%)';
+        element.style.color = 'white';
+      }
+      // if (
+      //   element.textContent &&
+      //   element.textContent.includes('Squash and merge')
+      // ) {
+      //   element.textContent = 'SMERGE!';
+      // }
+      // if (
+      //   element.textContent &&
+      //   element.textContent.includes('Confirm squash and merge')
+      // ) {
+      //   element.textContent = 'Confirm SMERGE!';
+      // }
+    }
+  }
 }
 
 function worker() {
@@ -69,4 +69,3 @@ function worker() {
 }
 
 setInterval(worker, 2000);
-
