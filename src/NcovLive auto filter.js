04dc3e8c64fecf07767801fbf46a0337b9c3dc10
George Gillams <george.gillams@skyscanner.net>
2020-03-16 11:09:17 +0000

    [NO-JIRA] Update scripts

--- src/NcovLive auto filter.js
+++ src/NcovLive auto filter.js
@@ -3,19 +3,24 @@
 // @namespace   urn://https://www.georgegillams.co.uk/greasemonkey/ncovlive_auto_filter
 // @include     *ncov2019.live*
 // @exclude     none
-// @version     1
+// @version     2
 // @description:en	Auto filters for `united kingdom` in the global data
 // @grant    		none
 // @description   	Auto filters for `united kingdom` in the global data
 // ==/UserScript==
 
+let searchDone = false;
+
 function setFilter() {
   const inputField = document.getElementById('sortable_table_Global_filter')
     .children[0].children[0];
-  inputField.value = 'united kingdom';
+  if (inputField && !searchDone) {
+    inputField.value = 'united kingdom';
 
-  // Trigger an input change event otherwise it won't realise the input has changed 😞
-  inputField.dispatchEvent(new Event('search'));
+    // Trigger an input change event otherwise it won't realise the input has changed 😞
+    inputField.dispatchEvent(new Event('search'));
+    searchDone = true;
+  }
 }
 
 function worker() {
