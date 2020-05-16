ae546f36d14633c83003ad547b7206574c17adb9
George Gillams <george.gillams@skyscanner.net>
2020-05-16 17:40:22 +0100

    [NO-JIRA] Simplify play pause logic

--- src/Disney Plus Play Pause.js
+++ src/Disney Plus Play Pause.js
@@ -3,7 +3,7 @@
 // @namespace   urn://https://www.georgegillams.co.uk/greasemonkey/disney_plus_play_pause
 // @include     *disneyplus.com*
 // @exclude     none
-// @version     1
+// @version     2
 // @description:en	Allows you to play/pause disney plus by pressing `space` on your keyboard
 // @grant    		none
 // @description Allows you to play/pause disney plus by pressing `space` on your keyboard
@@ -12,11 +12,7 @@
 function addListener() {
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 32) {
-      // The play/pause button is not available unless we activate the playback controls:
       document.getElementsByClassName('btm-media-client-element')[0].click();
-
-      // Now we can click the play/pause button
-      document.getElementsByClassName('play-pause-icon')[0].click();
     }
   });
 }
