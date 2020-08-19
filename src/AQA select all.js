047041a47826f4545d64c49fd5ed57ffbe977521
George Gillams <george.gillams@skyscanner.net>
2020-08-19 15:29:33 +0100

    [NO-JIRA] Add Usablenet script 2

---
+++ src/AQA select all.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name        AQA select all
+// @namespace   urn://https://www.georgegillams.co.uk/api/greasemonkey/aqa_select_all
+// @include     *usablenet.com/*
+// @exclude     none
+// @version     1
+// @description:en	Allows you to select all issues currently visible
+// @grant    		none
+// @description	Allows you to select all issues currently visible
+// ==/UserScript==
+
+const SELECT_ALL_BUTTON_ID = 'SELECT_ALL_BUTTON_ID_92hotv782';
+
+function onSelectAll() {
+  const categoryElements = document.getElementsByClassName('sc-ruleIssue');
+  console.log(`categoryElements`, categoryElements);
+
+  for (let i = 0; i < categoryElements.length; i += 1) {
+    const categoryElement = categoryElements[i];
+    const checkboxes = categoryElement.getElementsByTagName('INPUT');
+    for (let j = 0; j < checkboxes.length; j += 1) {
+      const checkbox = checkboxes[j];
+      checkbox.click();
+    }
+  }
+}
+
+function addButtonIfNecessary() {
+  const currentButton = document.getElementById(SELECT_ALL_BUTTON_ID);
+  if (currentButton) {
+    return;
+  }
+
+  const manualReviewControlPanels = document.getElementsByClassName(
+    'manual-review-filters',
+  );
+  if (!manualReviewControlPanels || manualReviewControlPanels.length < 1) {
+    return;
+  }
+
+  const manualReviewControlPanel = manualReviewControlPanels[0];
+
+  const selectButton = document.createElement('button');
+  selectButton.innerText = `Toggle all`;
+  selectButton.style.color = 'white';
+  selectButton.style.fontSize = '1rem';
+  selectButton.style.fontWeight = 'bold';
+  selectButton.style.padding = '0.125rem 1.125rem';
+  selectButton.id = SELECT_ALL_BUTTON_ID;
+  selectButton.style.border = 'none';
+  selectButton.style.backgroundColor = '#0770E3';
+  selectButton.style.borderRadius = '0.25rem';
+  // eslint-disable-next-line
+  selectButton.onclick = onSelectAll;
+
+  manualReviewControlPanel.appendChild(selectButton);
+}
+
+function worker() {
+  try {
+    addButtonIfNecessary();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+}
+
+setInterval(worker, 1500);
