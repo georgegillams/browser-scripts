008a82db7181a4ac7741cd342b0353ed4f02062e
George Gillams <george.gillams@skyscanner.net>
2020-12-06 17:14:57 +0000

    [NO-JIRA] Update scripts

---
+++ src/Google Meet auto admit.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Google Meet auto-admit
+// @namespace   urn://https://www.georgegillams.co.uk/api/greasemonkey/google_meet_auto_admit
+// @include     *meet.google.com*
+// @exclude     none
+// @version     0.0.1
+// @description:en	Automatically admits Google Meet participants
+// @grant    		none
+// @description Automatically admits Google Meet participants
+// ==/UserScript==
+
+function admitUserIfWaiting() {
+  const divElements = document.getElementsByTagName('DIV');
+  for (let i = 0; i < divElements.length; i += 1) {
+    const div = divElements[i];
+    if (div.innerText === 'Admit') {
+      console.log(`Admitting participant automatically.`);
+      div.click();
+    }
+  }
+}
+
+function worker() {
+  try {
+    admitUserIfWaiting();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+}
+
+setInterval(worker, 2000);
